ba0b32e11164acdd76e012d1027f41492bc4bf8c
et <42124348+et0and@users.noreply.github.com>
2019-03-02 13:22:57 +1300

    Update main.js

--- aai/main.js
+++ aai/main.js
@@ -7,4 +7,10 @@ function showMore( link, object ) {
 $( document ).ready( function() {
 	showMore( '.toggle', '.information' );
 	showMore( '.toggle2', '.information2' );
+	showMore( '.toggle3', '.information3' );
+	showMore( '.toggle4', '.information4' );
+	showMore( '.toggle5', '.information5' );
+	showMore( '.toggle6', '.information6' );
+	showMore( '.toggle7', '.information7' );
+	showMore( '.toggle8', '.information8' );
 });
